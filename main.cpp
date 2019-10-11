95240007030ea8fbbc751ec6b36e301d5db4df9d
valeriaacoronaa <vcorona@mail.csuchico.edu>
2019-10-11 12:42:33 -0700

    creates a grocery list

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main()
   string userItems;
   int numItems = 0;
   char input= ' ';
-
+  
   do
   {
     cout<<"\n==GROCERY LIST MANAGER==";
@@ -21,19 +21,27 @@ int main()
     cin.ignore();
     if(input == 'A' || input == 'a')
     {
-      cout<<"What is the item?\n";
-      cin>>userItems;
-      cin.ignore();
-      if(numItems >= 5)
-      {
-        cout<<"You'll need a bigger list!\n";
-      }else
-      {
-      numItems++;
-      }
-    }
-   
-   }while(input!= 'Q' && input!= 'q');
+        cout<<"What is the item?\n";
+        cin>>userItems;
+        cin.ignore();
 
+        if(numItems >= 5)
+        {
+          cout<<"You'll need a bigger list!\n";
+        }else
+        {
+          list[numItems] = userItems;
+          numItems++;
+        }
+    }
+   }while(input!= 'Q' && input!= 'q');
+ 
+  cout<<"==ITEMS TO BUY==\n";
+    for(numItems=0; numItems<5; numItems++)
+    {
+      cout<<numItems+1<<' ';
+      cout<<list[numItems]<<endl;
+    }
+    
   return 0;
 }
